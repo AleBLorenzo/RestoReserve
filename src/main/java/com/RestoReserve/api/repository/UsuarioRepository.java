c515368094a45e6e3dd125ab65086a056f69ca23
AleBLorenzo <alejandrobl0819@gmail.com>
2026-05-07 19:55:24 +0100

    feat: Agregar servicios para las entidades

--- src/main/java/com/RestoReserve/api/repository/UsuarioRepository.java
+++ src/main/java/com/RestoReserve/api/repository/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package com.RestoReserve.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,6 @@ import com.RestoReserve.api.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
 }
