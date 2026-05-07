851f1bd9e53d4ffd38d0d6bfdf5444a2a9818438
AleBLorenzo <alejandrobl0819@gmail.com>
2026-05-07 19:06:38 +0100

    Agregar repositorios para las entidades Mesa, Reserva, Trabajador y Usuario

---
+++ src/main/java/com/RestoReserve/api/repository/MesaRepository.java
@@ -0,0 +1,16 @@
+package com.RestoReserve.api.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.RestoReserve.api.model.Mesa;
+
+@Repository
+public interface MesaRepository extends JpaRepository<Mesa, Long> {
+
+    Optional<Mesa> findByNumeroDeMesa(int numeroDeMesa);
+
+    Optional<Mesa> findByCapacidad(int capacidad);
+}
